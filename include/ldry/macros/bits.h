f9b2b7b68ae8ec47b40fe5cab314aa1e8764ce4e
Lucian Carata <lc525@cam.ac.uk>
2017-07-19 03:58:09 +0100

    Fix macro name colision (already defined in the linux kernel)

--- include/ldry/macros/bits.h
+++ include/ldry/macros/bits.h
@@ -3,6 +3,6 @@
 
 #define SET_BIT(x) 1U << x
 #define SET_ALL_BITS(x) ((1U << (x)) - 1)
-#define BIT_MASK(x, y) (-1U >> (31 - (y))) & ~((1U << (x)) - 1)
+#define INTXN_BIT_MASK(x, y) (-1U >> (31 - (y))) & ~((1U << (x)) - 1)
 
 #endif
